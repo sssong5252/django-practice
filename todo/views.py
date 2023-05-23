f872c9703f29964d149d0df9b413b5db2b23f8a6
sssong5252 <luckyjude@naver.com>
2023-05-23 20:18:57 +0900

    Todo Create기능 구현하기

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from todo.models import TodoList
 
 # Create your views here.
+
+def create_todo(request):
+	myTodo = TodoList()
+	if request.method == 'POST':
+		myTodo.todo = request.POST['todo']
+		myTodo.description = request.POST['description']
+		myTodo.important = request.POST.get('important') == "on"
+		myTodo.complete = request.POST.get('complete') == "on"
+		myTodo.save()
+		return redirect('todos')
+	return render(
+		request,
+		'todo/todo_create.html'
+	)
+ 
 def todos(request):
 	todolist = TodoList.objects.all()
 	return render(
